3f083c31cd5c5728e673e75330301abf247d689d
Marius Pop <30757467+mariusCristianPop@users.noreply.github.com>
2022-01-31 21:31:41 +0100

    created html structure

---
+++ js/myScript.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    //document.getElementById("buttonsList").style.visibility = "hidden";
+});
+
+
+function validateEntry(){
+}
+
+function showDiv(){
+
+}
