2659daa1df2835048cefe6a7735002f15af320d1
Marius Pop <30757467+mariusCristianPop@users.noreply.github.com>
2022-02-02 18:57:45 +0100

    Changes

--- js/myScript.js
+++ js/myScript.js
@@ -1,11 +1,54 @@
-document.addEventListener("DOMContentLoaded", function() {
-    //document.getElementById("buttonsList").style.visibility = "hidden";
-});
 
-
-function validateEntry(){
+var userInsertedNr;
+// create the 3 buttons and append them to body
+function createButtons() {
+    document.getElementById("inputField").style.visibility = "hidden"; 
+    var myDiv = document.getElementById("buttonsList");
+    userInsertedNr = document.getElementById("buttonsNr").value;
+    drawWinner(userInsertedNr);
+    for (let i = 0; i < userInsertedNr; ++i) {
+        var button = document.createElement('button');
+        button.innerHTML = "Button " + (i + 1);
+        button.id = i;
+        button.type = "submit"
+        button.setAttribute("onclick", `checkWinner(${i})`);
+        myDiv.appendChild(button);
+    }
+}
+// Decide wich ID won the game
+var winner = -1;
+function drawWinner(userInsertedNr) {
+    winner = Math.floor(Math.random() * userInsertedNr) + 1;
+    console.log("The winner is " + winner); // left here on purpose for debugging reasons
 }
 
-function showDiv(){
+// Check if the user clicked on the winning button
+function checkWinner(x) {
+    if (x == winner) {
+       showResult(1);
+    } else {
+        showResult(0);
+    }
+}
 
+function showResult(parameter) {
+    var divElem = document.createElement('p');
+    divElem.setAttribute('style', 'text-align:center;');
+    divElem.setAttribute('name', 'resultDiv');
+    if (parameter == 1) {
+        var textNode = document.createTextNode("You clicked the lucky button!");
+        hideButtons();
+    } else {
+        var textNode = document.createTextNode("You missed!");
+        hideButtons(); 
+    }
+    divElem.appendChild(textNode);
+    document.body.appendChild(divElem);
+         
+}
+
+function hideButtons() {
+    for (let i = 0; i < userInsertedNr; ++i) {
+        document.getElementById(i).style.visibility = "hidden"; 
+    }
 }
